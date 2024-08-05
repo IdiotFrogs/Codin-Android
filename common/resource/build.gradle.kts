d26a9996ed6cf15167e0175a1486e9cb98ee19a5
sunghyun <pjhomg5530@naver.com>
2024-08-05 14:50:52 +0900

    :wrench: :: convention 적용

--- common/resource/build.gradle.kts
+++ common/resource/build.gradle.kts
@@ -36,8 +36,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso)
 }
