710cf052895fc71bd627888762a70627a4aadd5f
sunghyun <pjhomg5530@naver.com>
2024-08-22 19:15:12 +0900

    :wrench: :: detekt 의존성 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.baselineprofile) apply false
+    alias(libs.plugins.detekt)
 }
 
 allprojects {
@@ -30,5 +31,17 @@ allprojects {
                     )
             }
         }
+    }
+}
+
+subprojects { // TODO 추 후 libs.version.toml 파일에 접근하여 plugin, version 정보 가져오기
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    detekt{
+        toolVersion = "1.23.6"
+        buildUponDefaultConfig = true
+        allRules = false
+        config.setFrom("$rootDir/config/detekt/detekt.yml")
+        ignoreFailures = true // detekt 빌드시 실패 ignore 처리
     }
 }
