d55d1bffc05dd1d6a860b9cd5ec2028d88d48e0f
sunghyun <pjhomg5530@naver.com>
2024-09-09 17:01:56 +0900

    :fire: :: enableStrongSkippingMode 제거

--- build-logic/src/main/kotlin/com/example/architecture_test/ComposeAndroid.kt
+++ build-logic/src/main/kotlin/com/example/architecture_test/ComposeAndroid.kt
@@ -2,9 +2,11 @@ package com.example.architecture_test
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeFeatureFlag
 
 internal fun Project.configureComposeAndroid(commonExtension: CommonExtension<*, *, *, *, *, *>) {
     commonExtension.apply {
