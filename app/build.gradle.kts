e0e5848ac11ac1b37213839955eafb79f3a68b57
조현서 <s21035@gsm.hs.kr>
2024-10-14 16:13:05 +0900

    :sparkles: :: roborazzi dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,4 +12,7 @@ dependencies {
     androidTestImplementation(libs.androidx.test.espresso)
     baselineProfile(project(":baselineprofile"))
     implementation(libs.androidx.profileinstaller)
+    implementation(libs.roborazzi)
+    implementation(libs.roborazzi.compose)
+    implementation(libs.roborazzi.rule)
 }
