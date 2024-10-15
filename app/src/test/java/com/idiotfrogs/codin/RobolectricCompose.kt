5705e9ff5ff601933b7c73d5ee60c924f8400d01
조현서 <s21035@gsm.hs.kr>
2024-10-15 09:27:32 +0900

    :bug: :: 테스트 방식 변경

--- app/src/test/java/com/idiotfrogs/codin/RobolectricCompose.kt
+++ app/src/test/java/com/idiotfrogs/codin/RobolectricCompose.kt
@@ -1,11 +1,7 @@
 package com.idiotfrogs.codin
 
-import androidx.compose.material3.Text
 import androidx.compose.ui.test.hasText
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onRoot
-import com.github.takahirom.roborazzi.captureRoboImage
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,11 +12,16 @@ import org.robolectric.annotation.GraphicsMode
 @RunWith(RobolectricTestRunner::class)
 class RoborazziTest {
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<MainActivity>()
+    val composeTestRule = createComposeRule()
 
     @Test
     fun roborazziTest() {
-        composeTestRule.onRoot()
-            .captureRoboImage()
+        composeTestRule.setContent {
+            Greeting()
+        }
+
+        composeTestRule
+            .onNode(hasText("Hello"))
+            .assertExists()
     }
 }
